4af720c9ecdecd921c66f57775e35d52c0a59a37
giov-scar <giovanniscarantino@gmail.com>
2023-09-12 22:39:39 +0200

    adding commit

---
+++ glimm/src/app/pages/sculpture/sculpture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sculpture',
+  templateUrl: './sculpture.component.html',
+  styleUrls: ['./sculpture.component.scss']
+})
+export class SculptureComponent {
+
+}
