a363bd5c7b8ad1e18bd188cd110833f418abc8fb
giov-scar <giovanniscarantino@gmail.com>
2023-10-03 23:13:47 +0200

    typescript on nav

--- glimm/src/app/components/nav/nav.component.ts
+++ glimm/src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgbCollapseModule,NgbDropdownModule, NgbNavModule, } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
 
@@ -10,7 +10,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
+
 export class NavComponent {
   isMenuCollapsed = true;
+  isDropdownOpen = false;
+  isMobileMenuOpen = false;
+
+  @ViewChild('menuToggle')
+  menuToggle!: ElementRef<HTMLInputElement>;
+  @ViewChild('dropCheckbox')
+  dropCheckbox!: ElementRef<HTMLInputElement>;
+binding: any;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    this.isDropdownOpen = false; // Chiude il menu a tendina su mobile quando si apre il menu principale
+    if (this.menuToggle.nativeElement) {
+      this.menuToggle.nativeElement.checked = this.isMobileMenuOpen;
+    }
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeMenu() {
+    this.isMobileMenuOpen = false;
+    this.isDropdownOpen = false; // Chiude il menu a tendina su desktop quando si clicca su un elemento della lista
+    this.isMenuCollapsed = true;
+    if (this.menuToggle.nativeElement) {
+      this.menuToggle.nativeElement.checked = false;
+    }
+  }
+
 
 }
